f8d58a74d3dfbfad290c51d84641285b348dbeb2
AleCJunior <ale53campos@gmail.com>
2025-03-31 14:50:11 -0400

    Initial Commit

---
+++ WaterColorSorter/WaterColorSorterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterColorSorterApp.swift
+//  WaterColorSorter
+//
+//  Created by user272782 on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterColorSorterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaterColorSorterTests/WaterColorSorterTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaterColorSorterTests.swift
+//  WaterColorSorterTests
+//
+//  Created by user272782 on 3/25/25.
+//
+
+import Testing
+@testable import WaterColorSorter
+
+struct WaterColorSorterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
