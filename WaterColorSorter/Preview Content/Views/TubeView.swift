74e8488b8f76639517f9e953e75477544d9fa592
AleCJunior <ale53campos@gmail.com>
2025-04-07 14:07:20 -0400

    Added TubeView

---
+++ WaterColorSorter/Preview Content/Views/TubeView.swift
@@ -0,0 +1,18 @@
+//
+//  TubeView.swift
+//  WaterColorSorter
+//
+//  Created by user272782 on 4/7/25.
+//
+
+import SwiftUI
+
+struct TubeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TubeView()
+}
